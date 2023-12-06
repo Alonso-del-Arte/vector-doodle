761cf9035e7d6540a4233d6f204d85a35ff76b27
Alonso del Arte <alonso.delarte@gmail.com>
2023-12-06 01:38:52 -0500

    Passing all tests so far

--- test/graphicsrecord/MockGraphics.java
+++ test/graphicsrecord/MockGraphics.java
@@ -194,12 +194,12 @@ public class MockGraphics extends Graphics {
 
     @Override
     public Color getColor() {
-        return null;// this.currColor;
+        return this.currColor;
     }
 
     @Override
     public Font getFont() {
-        return null;// this.currFont;
+        return this.currFont;
     }
 
     // TODO: Write tests for this
@@ -289,10 +289,10 @@ public class MockGraphics extends Graphics {
      * @throws NullPointerException If <code>color</code> is null.
      */
     MockGraphics(Color color, Font font) {
-//        if (color == null || font == null) {
-//            String excMsg = "Neither color nor font can be null";
-//            throw new NullPointerException(excMsg);
-//        }
+        if (color == null || font == null) {
+            String excMsg = "Neither color nor font can be null";
+            throw new NullPointerException(excMsg);
+        }
         this.currColor = color;
         this.currFont = font;
     }
